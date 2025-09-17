6ab4115b5c43c68969b12ac04f6cd04023a1855b
Viktor Hetman <vitya.getman.79@gmail.com>
2025-09-17 13:35:17 +0200

    set up jest, configs for unit and e2e tests, unit test for the register

---
+++ src/auth/auth.controller.spec.ts
@@ -0,0 +1,65 @@
+import { BadRequestException } from '@nestjs/common'
+import { Test, TestingModule } from '@nestjs/testing'
+
+import { AuthController } from './auth.controller'
+import { AuthService } from './auth.service'
+import { RegisterDto } from './dto/register.dto'
+
+const newUser: RegisterDto = {
+	email: 'newuser@gmail.com',
+	name: 'new user',
+	password: '123456',
+	passwordRepeat: '123456'
+}
+
+describe('Auth Controller', () => {
+	let controller: AuthController
+	let service: AuthService
+
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			controllers: [AuthController],
+			providers: [
+				{
+					provide: AuthService,
+					useValue: {
+						register: jest.fn().mockResolvedValue(newUser)
+					}
+				}
+			]
+		}).compile()
+
+		controller = module.get<AuthController>(AuthController)
+		service = module.get<AuthService>(AuthService)
+	})
+
+	it('should be defined', () => {
+		expect(AuthController).toBeDefined()
+	})
+
+	it('should register new user', async () => {
+		const result = await controller.register(newUser)
+		expect(result).toEqual(newUser)
+	})
+
+	it('should throw bad request exception if invalid data was provided', async () => {
+		jest.spyOn(service, 'register').mockRejectedValueOnce(
+			new BadRequestException()
+		)
+
+		try {
+			const invalidUser: RegisterDto = {
+				email: '123',
+				name: '123',
+				password: '123',
+				passwordRepeat: '123'
+			}
+
+			await controller.register(invalidUser)
+		} catch (err) {
+			expect(err).toBeInstanceOf(BadRequestException)
+			const error = err as BadRequestException
+			expect(error.message).toBe('Bad Request')
+		}
+	})
+})
